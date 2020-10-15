44c8ade0ed4b836d85d9c691220e3343976606a0
Duane Haworth <duanehaworth@hotmail.com>
2020-10-15 12:31:09 -0500

    Tweak UI

--- Assignment/MidTerm.py
+++ Assignment/MidTerm.py
@@ -6,6 +6,13 @@ def clear():
     else: # for mac and linux(here, os.name is 'posix') 
         _ = system('clear') 
 
+# https://stackoverflow.com/questions/287871/how-to-print-colored-text-in-python
+# https://en.wikipedia.org/wiki/ANSI_escape_code#SGR_.28Select_Graphic_Rendition.29_parameters
+class bcolors:
+    WARNING = '\033[31m'
+    SUCCESS = '\033[92m'
+    ENDC = '\033[0m'
+
 def writeToTextFile(filepath, product_details):
     with open(filepath, "a") as file:
         file.write(",".join(product_details) + "\n")
@@ -17,11 +24,12 @@ def readListFromTextFile(filepath):
     except FileNotFoundError:
         raise Exception("Could not find " + filepath + " file.")
 
-def showMenu():
+def showMenu(msg = ""):
     print("Inventory Program")
     print("1) Enter a product")
     print("2) Display all products")
     print("3) Quit")
+    print(msg)
 
 def get_price(prompt):
     while True:
@@ -44,25 +52,25 @@ def get_command(prompt, low, high):
                 return number
             else:
                 clear()
-                showMenu()
-                print("Entry must be greater than " + str(low) 
-                    + " and less than or equal to " + str(high) + ".")
+                showMenu(f"{bcolors.WARNING}Entry must be greater than {str(low)}and less than or equal to {str(high)}.{bcolors.ENDC}")
         except ValueError:
             clear()
-            showMenu()
-            print("You entered an invalid number")        
+            showMenu(f"{bcolors.WARNING}You entered an invalid number{bcolors.ENDC}")
 
 def main():
+    ENTER_PRODUCT = 1
+    DISPLAY_PRODUCTS = 2
+    QUIT = 3
+
     FILE_NAME = "Assignment\\mid-term-products.txt"
     clear()
 
-    while True:        
-        showMenu()
-        print()
+    showMenu()
+    while True:                
         command = get_command("Select a menu option: ", 0, 3)   
         print()     
-        if command == 1:
-
+        if command == ENTER_PRODUCT:
+            clear()
             print("Enter a product")
             product = []
             product.append(input("Name: "))
@@ -70,35 +78,30 @@ def main():
             product.append(str(get_price("Price: ")))
             writeToTextFile(FILE_NAME, product)
             clear()
+            showMenu(f"{bcolors.SUCCESS}Record Added{bcolors.ENDC}")
 
-        elif command == 2:
+        elif command == DISPLAY_PRODUCTS:
 
             try:
                 list = readListFromTextFile(FILE_NAME)
+                clear()
                 print("Products on file")
                 print("-------------------")
                 for i in range(len(list)):
                     print(str(i + 1) + ". " + list[i], end="")
                 print("-------------------")
+                print()
+                showMenu(f"{bcolors.SUCCESS}List above{bcolors.ENDC}")
 
             except Exception as e:
                 clear()
-                print(e)
+                showMenu(f"{bcolors.WARNING}{e}{bcolors.ENDC}")
 
-        # elif command == 2:
-        #     try:
-        #         print(readFromTextFile(FILE_NAME))            
-        #     except Exception as e:
-        #         clear()
-        #         print(e)
-
-        elif command == 3:
+        elif command == QUIT:
             print("The program has ended")
             break
         else:
-            print("Invalid command.")
-
-        print()
+            print("Invalid command.  Should never get here.")
 
 if __name__ == "__main__":
     main()            
